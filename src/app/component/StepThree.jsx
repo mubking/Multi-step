51f205405a2027f5585e73a02e4652b587f72103
mubking <adeshinamubarak6@gmail.com>
2023-08-05 20:51:26 +0100

    i want to pull

--- src/app/component/StepThree.jsx
+++ src/app/component/StepThree.jsx
@@ -2,20 +2,20 @@
 
 const StepThree = ({ activePick, handleClickPick, pickAdd }) => {
   return (
-    <div className="flex flex-row justify-between items-center mt-5 two bg-[#F8F9FE]">
+    <div className="flex flex-row justify-between items-center mt-5 two gap-5 bg-[#F8F9FE]">
       {pickAdd.map((pa, i) => (
         <div
           key={i}
           onClick={() => handleClickPick(pa)}
           className={`${
-            activePick === i ? "bg-blue-500" : ""
-          } w-[100%] flex flex-row gap-2 items-center border one border-gray-300 p-4 border-sm`}
+            activePick === i ? "" : ""
+          } w-[100%] flex flex-row gap-20 items-center border one border-gray-300 p-4 border-sm`}
         >
           <div className="">
             <input type="checkbox" name="" id="" />
           </div>
 
-          <div className="flex justify-between items-center">
+          <div className="flex justify-between two items-center">
             <div className="flex flex-col gap-2">
               <h2 className="text-[#001C41]">{pa.head}</h2>
               <p className="text-[#C4C5CD]">{pa.text}</p>
